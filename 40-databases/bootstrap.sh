5050c32eaf997018e2f03591a329dda09c85b4db
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-26 18:03:04 +0530

    updated .tf file

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,4 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/daws-84s/ansible-roboshop-roles.git -e component=$1 main.yaml
+ansible-pull -U https://github.com/BharathKumarReddy2103/Ansible-Roboshop-Project-Using-Roles.git -e component=$1 main.yaml
