05391c8bce6b868c69651fbb362d53e26b541ac3
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-30 14:13:26 +0530

    updated .tf file

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -88,7 +88,7 @@ module "rabbitmq" {
 
 module "catalogue" {
     #source = "../../terraform-aws-securitygroup"
-    source = "https://github.com/BharathKumarReddy2103/terraform-aws-securitygroup.git?ref=main"
+    source = "git::https://github.com/BharathKumarReddy2103/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
@@ -212,4 +212,40 @@ resource "aws_security_group_rule" "catalogue_backend_alb" {
   protocol          = "tcp"
   source_security_group_id = module.backend_alb.sg_id
   security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_http" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  source_security_group_id = module.catalogue.sg_id
+  security_group_id = module.mongodb.sg_id
 }
