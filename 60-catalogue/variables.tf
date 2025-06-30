5aef4a04a8e3ba97a0075b6c32d2561e96d4dc64
BharathKumarReddy <nbkumar2103@gmail.com>
2025-06-30 13:02:29 +0530

    updated .tf file

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,13 @@
+variable "project" {
+    default = "roboshop"
+}
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+variable "environment" {
+    default = "dev"
+}
