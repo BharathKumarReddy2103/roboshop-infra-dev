2c590cb748295202ea4e6f682f939973d4410414
BharathKumarReddy <nbkumar2103@gmail.com>
2025-08-16 19:20:51 +0530

    updated file

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -6,9 +6,9 @@ variable "environment" {
     default = "dev"
 }
 
-variable "zone_id" {
-  default = "Z09266885JREMC64NO1J"
-}
+# variable "zone_id" {
+#   default = "Z09266885JREMC64NO1J"
+# }
 
 variable "zone_name" {
   default = "bharath2103.site"
