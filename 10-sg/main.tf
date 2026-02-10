8902d173af9deed62ce5c90d0d612b48f22131a4
BharathKumarReddy <nbkumar2103@gmail.com>
2026-02-10 06:39:01 +0530

    updated file

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -97,16 +97,16 @@
 #     vpc_id = local.vpc_id
 # }
 
-# module "backend_alb" {
-#     #source = "../../terraform-aws-securitygroup"
-#     source = "git::https://github.com/BharathKumarReddy2103/terraform-aws-securitygroup.git?ref=main"
-#     project = var.project
-#     environment = var.environment
+module "backend_alb" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/BharathKumarReddy2103/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
 
-#     sg_name = "backend-alb"
-#     sg_description = "for backend alb"
-#     vpc_id = local.vpc_id
-# }
+    sg_name = "backend-alb"
+    sg_description = "for backend alb"
+    vpc_id = local.vpc_id
+}
 
 module "frontend" {
     #source = "../../terraform-aws-securitygroup"
@@ -624,7 +624,7 @@ resource "aws_security_group_rule" "bastion_laptop" {
 #   security_group_id = module.vpn.sg_id
 # }
 
-/* # backend ALB accepting connections from my bastion host on port no 80
+# backend ALB accepting connections from my bastion host on port no 80
 resource "aws_security_group_rule" "backend_alb_bastion" {
   type              = "ingress"
   from_port         = 80
@@ -632,4 +632,4 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   protocol          = "tcp"
   source_security_group_id = module.bastion.sg_id
   security_group_id = module.backend_alb.sg_id
-} */
+}
