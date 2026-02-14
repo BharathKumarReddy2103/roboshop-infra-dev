cdf2e609b0ea3a13d2599a5ec1dd387a52d16cb7
BharathKumarReddy <nbkumar2103@gmail.com>
2026-02-14 07:10:37 +0530

    updated file

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -119,16 +119,16 @@ module "frontend" {
     vpc_id = local.vpc_id
 }
 
-# module "frontend_alb" {
-#     #source = "../../terraform-aws-securitygroup"
-#     source = "git::https://github.com/BharathKumarReddy2103/terraform-aws-securitygroup.git?ref=main"
-#     project = var.project
-#     environment = var.environment
+module "frontend_alb" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/BharathKumarReddy2103/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
 
-#     sg_name = "frontend-alb"
-#     sg_description = "for frontend alb"
-#     vpc_id = local.vpc_id
-# }
+    sg_name = "frontend-alb"
+    sg_description = "for frontend alb"
+    vpc_id = local.vpc_id
+}
 
 module "bastion" {
     #source = "../../terraform-aws-securitygroup"
@@ -549,33 +549,33 @@ resource "aws_security_group_rule" "backend_alb_vpn" {
 #   security_group_id = module.frontend.sg_id
 # }
 
-# resource "aws_security_group_rule" "frontend_frontend_alb" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   source_security_group_id = module.frontend_alb.sg_id
-#   security_group_id = module.frontend.sg_id
-# }
+resource "aws_security_group_rule" "frontend_frontend_alb" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.frontend_alb.sg_id
+  security_group_id = module.frontend.sg_id
+}
 
 # #Frontend ALB
-# resource "aws_security_group_rule" "frontend_alb_http" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-#   security_group_id = module.backend_alb.sg_id
-# }
+resource "aws_security_group_rule" "frontend_alb_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.backend_alb.sg_id
+}
 
-# resource "aws_security_group_rule" "frontend_alb_https" {
-#   type              = "ingress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-#   security_group_id = module.frontend_alb.sg_id
-# }
+resource "aws_security_group_rule" "frontend_alb_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.frontend_alb.sg_id
+}
 
 # bastion accepting connections from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
