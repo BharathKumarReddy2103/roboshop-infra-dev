f8a56ccbca33ea94295e5e906f23a3b0a81fa2c7
BharathKumarReddy <nbkumar2103@gmail.com>
2026-05-07 06:40:31 +0530

    updated file

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -4,11 +4,11 @@ resource "aws_ssm_parameter" "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-# resource "aws_ssm_parameter" "public_subnet_ids" {
-#   name  = "/${var.project}/${var.environment}/public_subnet_ids"
-#   type  = "StringList"
-#   value = join(",", module.vpc.public_subnet_ids)
-# }
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.vpc.public_subnet_ids)
+}
 
 # resource "aws_ssm_parameter" "private_subnet_ids" {
 #   name  = "/${var.project}/${var.environment}/private_subnet_ids"
