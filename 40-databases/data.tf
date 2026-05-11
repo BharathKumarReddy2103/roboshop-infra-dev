7de0d2fe3b98968300744e44f739e4e8c21d9eb7
BharathKumarReddy <nbkumar2103@gmail.com>
2026-05-11 07:09:42 +0530

    updated file

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -18,9 +18,9 @@ data "aws_ami" "joindevops" {
   }
 }
 
-# data "aws_ssm_parameter" "mongodb_sg_id" {
-#   name = "/${var.project}/${var.environment}/mongodb_sg_id"
-# }
+data "aws_ssm_parameter" "mongodb_sg_id" {
+  name = "/${var.project}/${var.environment}/mongodb_sg_id"
+}
 
 # data "aws_ssm_parameter" "redis_sg_id" {
 #   name = "/${var.project}/${var.environment}/redis_sg_id"
@@ -34,6 +34,6 @@ data "aws_ami" "joindevops" {
 #   name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 # }
 
-# data "aws_ssm_parameter" "database_subnet_ids" {
-#   name = "/${var.project}/${var.environment}/database_subnet_ids"
-# }
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project}/${var.environment}/database_subnet_ids"
+}
