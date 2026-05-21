09e9c7da225d19af2d7675c0b92645c354a552f5
BharathKumarReddy <nbkumar2103@gmail.com>
2026-05-21 06:33:38 +0530

    updated file

--- 10-sg/parameters.tf
+++ 10-sg/parameters.tf
@@ -64,11 +64,11 @@ resource "aws_ssm_parameter" "frontend_sg_id" {
   value = module.frontend.sg_id
 }
 
-# resource "aws_ssm_parameter" "frontend_alb_sg_id" {
-#   name  = "/${var.project}/${var.environment}/frontend_alb_sg_id"
-#   type  = "String"
-#   value = module.frontend_alb.sg_id
-# }
+resource "aws_ssm_parameter" "frontend_alb_sg_id" {
+  name  = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+  type  = "String"
+  value = module.frontend_alb.sg_id
+}
 
 resource "aws_ssm_parameter" "bastion_sg_id" {
   name  = "/${var.project}/${var.environment}/bastion_sg_id"
