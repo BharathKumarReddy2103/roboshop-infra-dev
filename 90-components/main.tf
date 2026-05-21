4c4f1797f034bc28947e742bdf87c5097ea83f96
BharathKumarReddy <nbkumar2103@gmail.com>
2026-05-21 22:49:56 +0530

    updated file

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,5 @@
 module "component" {
     for_each = var.components
-    # source = "../../terraform-aws-roboshop"
     source = "git::https://github.com/BharathKumarReddy2103/terraform-aws-roboshop.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
