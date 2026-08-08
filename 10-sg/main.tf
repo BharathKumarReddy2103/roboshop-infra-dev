f16057d47e6a9e1e4fda9082b2440970cedcf5ff
BharathKumarReddy <nbkumar2103@gmail.com>
2026-08-08 08:02:04 +0530

    updated file

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -471,15 +471,15 @@ module "vpn" {
 #   security_group_id = module.payment.sg_id
 # }
 
-# #Backend ALB
-# resource "aws_security_group_rule" "backend_alb_vpn" {
-#   type              = "ingress"
-#   from_port         = 80
-#   to_port           = 80
-#   protocol          = "tcp"
-#   source_security_group_id = module.vpn.sg_id
-#   security_group_id = module.backend_alb.sg_id
-# }
+# Backend ALB
+resource "aws_security_group_rule" "backend_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.backend_alb.sg_id
+}
 
 # # resource "aws_security_group_rule" "backend_alb_bastion" {
 # #   type              = "ingress"
