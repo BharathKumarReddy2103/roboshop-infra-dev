423ace0e376246474cf579ef726b1e36e3a38063
BharathKumarReddy <nbkumar2103@gmail.com>
2026-08-09 05:31:02 +0530

    updated file

--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "vpn" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.vpn_sg_id]
   subnet_id = local.public_subnet_id
-  #key_name = "aws key pair" # make sure this key exist in AWS
-  key_name = aws_key_pair.openvpn.key_name
+  key_name = "aws key pair" # make sure this key exist in AWS
+  # key_name = aws_key_pair.openvpn.key_name
   user_data = file("openvpn.sh")
 
   tags = merge(
@@ -20,11 +20,11 @@ resource "aws_instance" "vpn" {
   )
 }
 
-resource "aws_route53_record" "vpn" {
-  zone_id = var.zone_id
-  name    = "vpn-${var.environment}.${var.zone_name}"
-  type    = "A"
-  ttl     = 1
-  records = [aws_instance.vpn.public_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "vpn" {
+#   zone_id = var.zone_id
+#   name    = "vpn-${var.environment}.${var.zone_name}"
+#   type    = "A"
+#   ttl     = 1
+#   records = [aws_instance.vpn.public_ip]
+#   allow_overwrite = true
+# }
