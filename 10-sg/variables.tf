cc9dfab7cde6f35fcd1ecf16f2363f4e370f8065
BharathKumarReddy <nbkumar2103@gmail.com>
2026-08-09 06:33:41 +0530

    updated file

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -26,14 +26,14 @@ variable "mongodb_ports_vpn" { #just keep as mongodb_ports
     default = [22, 27017]
 }
 
-# variable "redis_ports_vpn" {
-#     default = [22, 6379]
-# }
+variable "redis_ports_vpn" {
+    default = [22, 6379]
+}
 
-# variable "mysql_ports_vpn" {
-#     default = [22, 3306]
-# }
+variable "mysql_ports_vpn" {
+    default = [22, 3306]
+}
 
-# variable "rabbitmq_ports_vpn" {
-#     default = [22, 5672]
-# }
+variable "rabbitmq_ports_vpn" {
+    default = [22, 5672]
+}
